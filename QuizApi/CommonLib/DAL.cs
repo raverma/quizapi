f769e88b84109eb7c2427f5b80aa05b1173c2dc7
Lalitha Busireddy <bx00507464@techmahindra.com>
2020-02-24 12:38:16 +0530

    Update DAL.cs
    Added ExecuteNonQueryCommand method

--- QuizApi/CommonLib/DAL.cs
+++ QuizApi/CommonLib/DAL.cs
@@ -10,100 +10,112 @@ using System.Collections;
 
 namespace QuizApi.CommonLib
 {
-  public enum ParamType
-  {
-    Numeric = 0,
-    Varchar = 1,
-    Boolean = 2,
-    Date = 3
-  }
-
-  public class DAL
-  {
-    public object ExecuteProcedureScalar(string procedureName, ICollection<Parameter> paramCollection, string connectionString)
+    public enum ParamType
     {
-      using (SqlConnection Connection = new SqlConnection(connectionString))
-      {
-        using (SqlCommand cmd = new SqlCommand(procedureName, Connection))
-        {
-          cmd.CommandType = CommandType.StoredProcedure;
-          foreach (Parameter param in paramCollection)
-          {
-            cmd.Parameters.Add(param.GetParam());
-          }
-          
-          cmd.Connection.Open();
-          return cmd.ExecuteScalar();
-        }
-      }
+        Numeric = 0,
+        Varchar = 1,
+        Boolean = 2,
+        Date = 3
     }
 
-    public int ExecuteNonQuerySql(string sqlText, string connectionString)
+    public class DAL
     {
-      using (SqlConnection Connection = new SqlConnection(connectionString))
-      {
-        using (SqlCommand cmd = new SqlCommand(sqlText, Connection))
+        public object ExecuteProcedureScalar(string procedureName, ICollection<Parameter> paramCollection, string connectionString)
         {
-          cmd.CommandType = CommandType.Text;
-          cmd.Connection.Open();
-          return cmd.ExecuteNonQuery();
-        }
-      }
-    }
-
-    public int ExecuteProcedure(string procedureName, ICollection<Parameter> paramCollection, string connectionString)
-    {
-        using (SqlConnection Connection = new SqlConnection(connectionString))
-        {
-            using (SqlCommand cmd = new SqlCommand(procedureName, Connection))
+            using (SqlConnection Connection = new SqlConnection(connectionString))
             {
-                cmd.CommandType = CommandType.StoredProcedure;
-                foreach (Parameter param in paramCollection)
+                using (SqlCommand cmd = new SqlCommand(procedureName, Connection))
                 {
-                    cmd.Parameters.Add(param.GetParam());
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    foreach (Parameter param in paramCollection)
+                    {
+                        cmd.Parameters.Add(param.GetParam());
+                    }
+
+                    cmd.Connection.Open();
+                    return cmd.ExecuteScalar();
                 }
-                cmd.Connection.Open();
-                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int ExecuteNonQuerySql(string sqlText, string connectionString)
+        {
+            using (SqlConnection Connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(sqlText, Connection))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public int ExecuteProcedure(string procedureName, ICollection<Parameter> paramCollection, string connectionString)
+        {
+            using (SqlConnection Connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(procedureName, Connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    foreach (Parameter param in paramCollection)
+                    {
+                        cmd.Parameters.Add(param.GetParam());
+                    }
+                    cmd.Connection.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public DataTable ExecuteCommand(string sqlCommand, string connectionString)
+        {
+            DataTable dtResult = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlDataAdapter sqlDA = new SqlDataAdapter(sqlCommand, connection))
+                {
+                    sqlDA.Fill(dtResult);
+                    return dtResult;
+                }
+            }
+        }
+
+        public int ExecuteNonQueryCommand(string sqlText, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(sqlText, connection))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+
+        public class Parameter
+        {
+            SqlParameter _param;
+            public Parameter(string name, ParamType paramType, object value)
+            {
+                _param = new SqlParameter(name, value);
+                if (paramType == ParamType.Varchar)
+                    _param.SqlDbType = SqlDbType.VarChar;
+                else if (paramType == ParamType.Numeric)
+                    _param.SqlDbType = SqlDbType.Int;
+                else if (paramType == ParamType.Boolean)
+                    _param.SqlDbType = SqlDbType.Bit;
+                else
+                    throw new Exception("Sql Parameter currenty not supported");
+                _param.SqlValue = value;
+            }
+
+            public SqlParameter GetParam()
+            {
+                return _param;
             }
         }
     }
-
-    public DataTable ExecuteCommand(string sqlCommand, string connectionString)
-    {
-      DataTable dtResult = new DataTable();
-      using (SqlConnection connection = new SqlConnection(connectionString))
-      {
-        using (SqlDataAdapter sqlDA = new SqlDataAdapter(sqlCommand, connection))
-        {
-          sqlDA.Fill(dtResult);
-          return dtResult;
-        }
-      }
-    }
-
-    
-  }
-
-  public class Parameter
-  {
-    SqlParameter _param;
-    public Parameter(string name, ParamType paramType, object value)
-    {
-      _param = new SqlParameter(name, value);
-      if (paramType == ParamType.Varchar)
-        _param.SqlDbType = SqlDbType.VarChar;
-      else if (paramType == ParamType.Numeric)
-        _param.SqlDbType = SqlDbType.Int;
-      else if (paramType == ParamType.Boolean)
-        _param.SqlDbType = SqlDbType.Bit;
-      else
-        throw new Exception("Sql Parameter currenty not supported");
-      _param.SqlValue = value;
-    }
-
-    public SqlParameter GetParam() 
-    { 
-        return _param;
-    }
-  }
 }
